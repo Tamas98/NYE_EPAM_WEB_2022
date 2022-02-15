24eebfe4561e7009fabbc965fa7b553bf722e1fd
Tamas_Tarcsa <Tamas_Tarcsa@epam.com>
2022-02-15 10:53:13 +0100

    Added spring to the application and a configuration class.

--- src/main/java/org/example/service/converters/CoinToCoinViewConverter.java
+++ src/main/java/org/example/service/converters/CoinToCoinViewConverter.java
@@ -2,10 +2,14 @@ package org.example.service.transformer;
 
 import org.example.domain.coin.Coin;
 import org.example.view.model.CoinView;
+import org.springframework.cglib.core.Transformer;
+import org.springframework.core.convert.converter.Converter;
 
-public class CoinToCoinViewTransformer {
+public class CoinToCoinViewTransformer implements Converter<Coin, CoinView> {
 
+    @Override
     public CoinView convert(Coin coin) {
         return new CoinView(coin.getSymbol(), coin.getPriceChange(), coin.getPriceChangePercent(), coin.getOpenPrice(), coin.getHighPrice(), coin.getLowPrice());
     }
+    
 }
