fb788182a10bef50a56664f01a38c0ae22a276e9
Tamas_Tarcsa <Tamas_Tarcsa@epam.com>
2022-02-16 13:42:46 +0100

    Modified the main class.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,11 @@
 package org.example;
 
 import org.example.view.PrintCoins;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         ApplicationContext applicationContext = SpringApplication.run(Main.class, args);//init the context
